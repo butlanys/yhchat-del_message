9a015578192ef29102c3a71a0a59c96fb7a7c276
hqycloud <huangqiyuyyds@gmail.com>
2024-07-31 23:24:05 +0800

    modified:   main.py

--- main.py
+++ main.py
@@ -19,10 +19,10 @@ id_6 = os.getenv("ID_6")  # 撤回后的提示消息的表单id
 id_7 = os.getenv("ID_7")  # 使用管理员权限撤回开关的表单id
 id_8 = os.getenv("ID_8")  # 撤回所有带二维码的图片
 id_9 = os.getenv("ID_9")  # 白名单表单id
-id_10 = os.getenv("ID_10") # 黑名单表单id
+id_10 = os.getenv("ID_10")  # 黑名单表单id
 
 TOKEN = os.getenv("TOKEN")  # 可在官网后台获取
-ADMIN_TOKEN = os.getenv("ADMIN_TOKEN")  # 管理员的用户token
+ADMIN_TOKEN = os.getenv("ADMIN_TOKEN")  # 管理员token
 
 app = Flask(__name__)
 data_path = 'data.json'
@@ -30,7 +30,6 @@ tmp_dir = 'tmp'
 
 if not os.path.exists(tmp_dir):
     os.makedirs(tmp_dir)
-
 
 def yhchat_push(recvId, recvType, contentType, content):
     url = f"https://chat-go.jwzhd.com/open-apis/v1/bot/send?token={TOKEN}"
@@ -45,7 +44,6 @@ def yhchat_push(recvId, recvType, contentType, content):
     }
     response = requests.request("POST", url, headers=headers, data=payload)
     return json.loads(response.text)
-
 
 def yhchat_batch_push(recvIds, recvType, contentType, content):
     url = f"https://chat-go.jwzhd.com/open-apis/v1/bot/batch_send?token={TOKEN}"
@@ -75,10 +73,9 @@ def del_message(msgId, chatId):
     response = requests.request("POST", url, headers=headers, data=payload)
     return json.loads(response.text)
 
-
 def del_message_admin(msgId, chatId):
     start = ["0x12", "0x20"]
-    unknown_symbol = ""
+    unknown_symbol = "  "
     combined = unknown_symbol + chatId
     encoded_combined = base64.b64encode(combined.encode()).decode()
     data = ''.join(chr(int(value, 16)) for value in start) + msgId + base64.b64decode(encoded_combined).decode()
@@ -92,19 +89,16 @@ def del_message_admin(msgId, chatId):
         "token": ADMIN_TOKEN
     }
 
-    response = requests.post("https://chat-go.jwzhd.com/v1/msg/recall-msg", data=data, headers=headers)
+    response = requests.post("https://chat-go.jwzhd.com/v1/msg/recall-msg", data=data,headers=headers)
     return response
-
 
 def load_data():
     with open(data_path, 'r') as f:
         return json.load(f)
 
-
 def save_data(data):
     with open(data_path, 'w') as f:
         json.dump(data, f)
-
 
 def is_forbidden_url(url, forbidden_urls):
     for forbidden_url in forbidden_urls:
@@ -117,13 +111,11 @@ def is_forbidden_url(url, forbidden_urls):
                 return True
     return False
 
-
 def extract_urls_from_html(html_content):
     urls = re.findall(
         r'(?:(?:https?://)?(?:(?:[a-zA-Z0-9\-_.]+)\.)+(?:[a-zA-Z]{2,6})+(?:/[\w\-\.]*)*(?:\?[\w\-\.\=\&]*)?(?:\#[\w\-]+)?)',
         html_content)
     return urls
-
 
 def get_redirect_url(url, max_redirects=5):
     try:
@@ -138,12 +130,10 @@ def get_redirect_url(url, max_redirects=5):
         print(f"Error during redirection: {e}")
         return None
 
-
 def check_image_for_qr_code(image_url, image_name, forbidden_urls, recall_all_qr_images):
     try:
         response = requests.get(image_url)
-        image_name = image_url.split("/")[-1]
-        image_path = os.path.join(tmp_dir, image_name)
+        image_path = os.path.join(tmp_dir, os.path.basename(image_name))
         with open(image_path, 'wb') as f:
             f.write(response.content)
 
@@ -154,12 +144,10 @@ def check_image_for_qr_code(image_url, image_name, forbidden_urls, recall_all_qr
             qr_url = barcode.parsed
 
             if recall_all_qr_images:
-                os.remove(image_path)
                 return True, qr_url
 
             final_url = get_redirect_url(qr_url)
             if final_url and is_forbidden_url(final_url, forbidden_urls):
-                os.remove(image_path)
                 return True, final_url
 
             if 'http' not in qr_url:
@@ -170,17 +158,18 @@ def check_image_for_qr_code(image_url, image_name, forbidden_urls, recall_all_qr
                 urls_in_html = extract_urls_from_html(html_content)
                 for url_in_html in urls_in_html:
                     if is_forbidden_url(url_in_html, forbidden_urls):
-                        os.remove(image_path)
                         return True, url_in_html
         else:
             print("此图像没有二维码")
 
-        os.remove(image_path)
         return False, None
 
     except Exception as e:
         print(f"扫描图像中的二维码失败: {e}")
         return False, None
+    finally:
+        if os.path.exists(image_path):
+            os.remove(image_path)
 
 
 def check_forbidden_word(content, forbidden_words):
@@ -188,7 +177,6 @@ def check_forbidden_word(content, forbidden_words):
         if re.search(word, content, re.IGNORECASE):
             return True, word
     return False, None
-
 
 def handle_message(json_data):
     event_type = json_data.get("header", {}).get("eventType")
@@ -218,8 +206,34 @@ def handle_message(json_data):
         if user_id in white_list:
             return
 
-        if forbidden_words and (content_type == "text" or content_type == "markdown"):
-            content = json_data["event"]["message"]["content"]["text"]
+        content = json_data["event"]["message"]["content"].get("text", "")
+
+        if "parent" in json_data["event"]["message"]["content"]:
+            parent_content = json_data["event"]["message"]["content"]["parent"]
+            is_parent_forbidden, matched_word_parent = check_forbidden_word(parent_content,forbidden_words)
+            if is_parent_forbidden:
+                if use_admin_token:
+                    del_message_admin(msg_id, chat_id)
+                else:
+                    del_message(msg_id, chat_id)
+                warn_message = data.get(chat_id, {}).get(id_6, {}).get("value", "")
+                if warn_message:
+                    formatted_warn_message = warn_message.format(user_name=user_name,word=matched_word_parent)
+                    yhchat_push(chat_id, "group", "text",{"text": formatted_warn_message})
+                else:
+                    yhchat_push(chat_id, "group", "text",{"text": "你发送的消息中引用的内容包含违规词，已被自动撤回"})
+                owner_ids = data.get(chat_id, {}).get(id_2, {}).get("value", "").split("\n")
+                group_name = data.get(chat_id, {}).get(id_3, {}).get("value", "")
+                if owner_ids:
+                    message = f"群 [{group_name}({chat_id}) 中的一条消息因引用内容包含违规词被撤回\n" \
+                              f"被撤回用户：{user_name}\n" \
+                              f"被撤回用户id：{user_id}\n" \
+                              f"原消息内容：{content}\n" \
+                              f"命中的违规词：{matched_word_parent}"
+                    yhchat_batch_push(owner_ids, "user", "markdown", {"text": message})
+
+        if content_type == "post":
+            content = json_data["event"]["message"]["content"]["postContent"]
             is_forbidden, matched_word = check_forbidden_word(content, forbidden_words)
             if is_forbidden:
                 if use_admin_token:
@@ -228,7 +242,30 @@ def handle_message(json_data):
                     del_message(msg_id, chat_id)
                 warn_message = data.get(chat_id, {}).get(id_6, {}).get("value", "")
                 if warn_message:
-                    formatted_warn_message = warn_message.format(user_name=user_name, word=matched_word)
+                    formatted_warn_message = warn_message.format(user_name=user_name,word=matched_word)
+                    yhchat_push(chat_id, "group", "text",{"text": formatted_warn_message})
+                else:
+                    yhchat_push(chat_id, "group", "text",{"text": "你发送的文章内容包含违规词，已被自动撤回"})
+                owner_ids = data.get(chat_id, {}).get(id_2, {}).get("value", "").split("\n")
+                group_name = data.get(chat_id, {}).get(id_3, {}).get("value", "")
+                if owner_ids:
+                    message = f"群 [{group_name}({chat_id}) 中的一条文章因包含违规词被撤回\n" \
+                              f"被撤回用户：{user_name}\n" \
+                              f"被撤回用户id：{user_id}\n" \
+                              f"文章内容：{content}\n" \
+                              f"命中的违规词：{matched_word}"
+                    yhchat_batch_push(owner_ids, "user", "markdown", {"text": message})
+
+        elif forbidden_words and (content_type == "text" or content_type == "markdown"):
+            is_forbidden, matched_word = check_forbidden_word(content, forbidden_words)
+            if is_forbidden:
+                if use_admin_token:
+                    del_message_admin(msg_id, chat_id)
+                else:
+                    del_message(msg_id, chat_id)
+                warn_message = data.get(chat_id, {}).get(id_6, {}).get("value", "")
+                if warn_message:
+                    formatted_warn_message = warn_message.format(user_name=user_name,word=matched_word)
                     yhchat_push(chat_id, "group", "text", {"text": formatted_warn_message})
                 else:
                     yhchat_push(chat_id, "group", "text", {"text": "你发送的消息包含违规词，已被自动撤回"})
@@ -249,8 +286,8 @@ def handle_message(json_data):
             else:
                 image_name = json_data["event"]["message"]["content"]["imageName"]
                 image_url = f"https://chat-img.jwznb.com/{image_name}"
-            is_forbidden, matched_url = check_image_for_qr_code(image_url, image_name, forbidden_urls,
-                                                                 recall_all_qr_images)
+
+            is_forbidden, matched_url = check_image_for_qr_code(image_url, image_name, forbidden_urls, recall_all_qr_images)
             if is_forbidden:
                 if use_admin_token:
                     del_message_admin(msg_id, chat_id)
